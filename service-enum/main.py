2d9df01bef73b8f178b24ee51ac66c414591267b
MuratZamir <zamirpy@gmail.com>
2024-09-16 15:40:03 -0700

    service enum capability

---
+++ service-enum/main.py
@@ -0,0 +1,89 @@
+from vulnscanner import Scanner
+import sys
+import pyfiglet
+import time
+import re
+import asyncio
+
+
+async def main():
+    ascii_banner = pyfiglet.figlet_format("PORTPY")
+    print(ascii_banner)
+    
+    rangePattern = re.compile(r'^\d+-\d+$')
+    portPattern = re.compile(r'^\d+(,\d+)*$')
+
+    try:
+        
+        targetIp = sys.argv[1]
+        portNumber = sys.argv[2]
+
+        print('Scanning is in progress...\n')
+
+        maxConcurrentTasks = 100  # Adjust based on your system's capacity
+        semaphore = asyncio.Semaphore(maxConcurrentTasks)
+
+        # Initialize Scanner object
+        scanner = Scanner(targetIp, semaphore)
+
+
+        if rangePattern.match(portNumber):
+            startPort, endPort = map(int, portNumber.split('-'))
+            startPort, endPort = min(startPort, endPort), max(startPort, endPort) # sorts the list if port range is given reverse order
+            ports = range(startPort, endPort+1) # port range in a list
+            
+
+        elif portPattern.match(portNumber):
+            ports = list(map(int, portNumber.split(','))) # for comma separated ports
+
+        else:
+            ports = [int(portNumber)] # single port as a list
+
+        print(f"{'PORT':<7}{'STATE':<6}{'SERVICE':<10}")        
+
+        tasks = [scanner.scanPort(port) for port in ports]
+        await asyncio.gather(*tasks)
+           
+        print(f'\nTotal open ports: {scanner.openPortCount}')
+
+
+        '''Further service enumeration with NMAP'''
+        furtherEnum = input('\nFurther service enum? [y/n] ')
+        
+        
+
+        if furtherEnum.lower() == 'y':
+            allPorts = input('Service enum for all open ports? [y/n] ')
+            # serviceName = scanner.portServices.get(scanner.portServices, 'unknown')
+            
+            if allPorts.lower() == 'y':
+                
+                print(f"\n{'PORT':<7}{'VERSION':<10}")
+                for port in scanner.nmapScanPorts:
+                    serviceInfo = scanner.nmap(str(port))
+                    print(f"{port:<7}{serviceInfo:<23}")
+
+            elif allPorts.lower() == 'n':
+                specificPorts = input('Specific port(s): [comma-separated] ')
+                specificPortsList = map(int, specificPorts.split(','))  # Convert to a list of integers
+                print(f"\n{'PORT':<7}{'VERSION':<10}")
+                for port in specificPortsList:
+                    serviceInfo = scanner.nmap(str(port))
+                    
+                    print(f"{port:<7}{serviceInfo:<23}")
+            
+
+    except (IndexError, ValueError):
+        print('[!] Error\nUsage: ./main.py <ip> <port/range>\nExample: ./main.py 10.0.0.1 1-100')
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    
+    asyncio.run(main())
+
+    finish = time.perf_counter()
+    elapsedTime = (finish - start)/60
+    
+    print(f'\nFinished in {round(elapsedTime, 2)} minutes')
+
+
---
+++ service-enum/vulnscanner.py
@@ -0,0 +1,95 @@
+from IPy import IP
+import sys
+import socket
+import asyncio
+import nmap
+import json
+
+
+class Scanner:
+
+    def __init__(self, target, semaphore):
+        self.target = target
+        self.semaphore = semaphore
+        self.portServices = {
+        21: 'ftp', 22: 'ssh', 23: 'telnet',
+        25: 'smtp', 53: 'dns', 80: 'http',
+        110: 'pop3', 111: 'rcpbind', 139: 'netbios-ssn',
+        143: 'imap', 443: 'https', 445: 'microsoft-ds',
+        512: 'exec',513: 'login', 514: 'shell', 631: 'ipp',
+        3306: 'mysql', 3389: 'rdp', 5432: 'postgresql',
+        5900: 'vnc', 6000: 'X11', 8834: 'nessus-xmlrpc', 33060: 'mysqlx'
+        # add more ports and services as needed
+        }
+        self.openPortCount = 0 # open port counter
+        self.lock = asyncio.Lock() # Lock to ensure safe access to counter
+        self.nmapScanPorts = []
+
+
+    def ipToDomain(self):
+        '''Function to resolve IP addresses'''
+        domain = socket.gethostbyname(self.target)
+        return domain
+
+
+    def validateIp(self):
+        '''Function to check if the IP is valid or not'''
+        try:
+            ipAddress = IP(self.target)
+            return str(ipAddress)
+        except ValueError:
+            print(f'{self.target} is not a valid IP address.')
+            sys.exit(1)
+    
+
+
+    async def scanPort(self, port):
+        '''Async function to check if a port is open or not'''
+        async with self.semaphore: # limits simultaneous network connections
+            try: 
+
+                convertedIp = str(self.validateIp())
+
+                '''Returns tuple containing two objects'''
+                # StreamReader for reading data from async connection
+                # StreamWriter for writing data to async connection
+                reader, writer = await asyncio.wait_for(asyncio.open_connection(convertedIp, port), timeout=2)
+
+                
+                serviceName = self.portServices.get(port, 'unknown')
+                print(f"{port}/tcp open  {serviceName}")
+                self.nmapScanPorts.append(port) # counting the open ports for nmap -sV
+
+                async with self.lock:
+                    self.openPortCount +=1
+
+                writer.close()
+                await writer.wait_closed()
+
+
+            except (asyncio.TimeoutError, ConnectionRefusedError, OSError):
+                pass
+            # uncomment if further information is needed
+                # serviceName = self.portServices.get(port, 'unknown')
+                # print(f"{port}/tcp closed  {serviceName} ({e})")
+      
+
+    def nmap(self, port):
+        nm = nmap.PortScanner()
+        try:
+            versionResult = nm.scan(self.target, str(port), arguments='-sV')
+
+            # print("Raw Nmap Output:", versionResult)
+            parsedResult = versionResult['scan'][self.target]['tcp'][int(port)]
+
+            name = parsedResult.get('name', 'unknown')
+            product = parsedResult.get('product', 'unknown')
+            version = parsedResult.get('version', 'unknown')
+            extraInfo = parsedResult.get('extrainfo', 'unknown')
+            # cpe = parsedResult['cpe']
+
+            result = f'{product} {version} {extraInfo}'
+            return result
+        
+        except KeyError:
+            return "no version information available"
